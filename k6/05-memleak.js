d18d04138dcb640e7a71871b6f30ee0b57325a91
Kaushal Dalvi <kaushaldalvi@gmail.com>
2023-04-27 16:36:12 -0400

    update versions

--- k6/05-memleak.js
+++ k6/05-memleak.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-    vus: 2,
-    duration: '60s',
+    vus: 20,
+    duration: '600s',
 };
 export default function () {
     const res = http.get('http://localhost:8080/process-order/');
